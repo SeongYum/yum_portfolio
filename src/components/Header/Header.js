6535e34449e1c7565e61dc8433d8aaddb1db0fe8
SeongYum <dbal6930@gmail.com>
2023-01-02 14:55:37 +0900

    반응형 헤더 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 import { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-scroll';
-
+import media from './../../styles/media';
+import { IoMenu, IoClose } from 'react-icons/io5';
 function Header() {
   const menuList = ['ABOUT ME', 'SKILLS', 'PROJECTS'];
 
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [BtnInfo, setBtnInfo] = useState(true);
+
+  const menuClick = () => {
+    if (BtnInfo === true) {
+      setBtnInfo(false);
+    } else if (BtnInfo === false) {
+      setBtnInfo(true);
+    }
+  };
 
   const updateScroll = useCallback(() => {
     setScrollPosition(window.scrollY);
@@ -22,11 +32,20 @@ function Header() {
     <HeaderContainer scrollPosition={scrollPosition}>
       <HeaderContents>
         <div className="name">
-          <Link to="Main" spy={true} smooth={true} duration={500}>
+          <Link
+            to="Main"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={() => setBtnInfo(true)}
+          >
             Yum&apos;s Portfolio
           </Link>
         </div>
-        <div className="menuList">
+        <MenuBtn onClick={menuClick}>
+          {BtnInfo ? <IoMenu /> : <IoClose />}
+        </MenuBtn>
+        <MenuList BtnInfo={BtnInfo}>
           {menuList.map((menu) => {
             return (
               <Link
@@ -36,11 +55,11 @@ function Header() {
                 duration={500}
                 key={menu}
               >
-                <div className="manuEl">{menu}</div>
+                <MeneEl onClick={() => setBtnInfo(true)}>{menu}</MeneEl>
               </Link>
             );
           })}
-        </div>
+        </MenuList>
       </HeaderContents>
     </HeaderContainer>
   );
@@ -50,16 +69,17 @@ export default Header;
 
 const HeaderContainer = styled.header`
   position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  /* width: 100%; */
   background-color: ${(props) =>
     props.scrollPosition ? '#d8a19f' : 'transparents'};
   transition: background-color 0.2s;
-  height: 65px;
+  width: 100%;
+  height: auto;
   display: flex;
   justify-content: center;
+  z-index: 10;
+  ${media.medium`
+  background-color: #d8a19f;
+       `}
 `;
 const HeaderContents = styled.div`
   width: 100%;
@@ -67,24 +87,58 @@ const HeaderContents = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 30px;
+  flex-wrap: wrap;
+  padding: 1.4rem 2rem;
+  overflow: hidden;
   .name {
     color: #ffffff;
     font-weight: 600;
     font-size: 1.5rem;
     cursor: pointer;
   }
-  .menuList {
-    display: flex;
-    .manuEl {
-      cursor: pointer;
-      white-space: nowrap;
-      margin-left: 50px;
-      color: #ffffff;
-      :hover {
-        transform: scale(1.07);
-        transition: 0.5s;
-      }
-    }
+  ${media.medium`
+     padding: 1.5rem 2rem 1.5rem;
+       `}
+`;
+const MenuList = styled.div`
+  display: flex;
+  transition: max-height 0.2s ease;
+  ${media.medium`
+    visibility: ${(props) => (props.BtnInfo ? 'hidden' : 'visible')};
+    flex-direction: column;
+    width: 100%;
+    max-height: ${(props) => (props.BtnInfo ? '0' : '30rem')};
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+    text-transform: capitalize;
+       `}
+`;
+const MeneEl = styled.div`
+  cursor: pointer;
+  white-space: nowrap;
+  margin-left: 50px;
+  color: #ffffff;
+  :hover {
+    transform: scale(1.07);
+    transition: 0.5s;
   }
+  ${media.medium`
+    margin-top: 2rem;
+       `}
+`;
+const MenuBtn = styled.button`
+  display: none;
+  width: 2rem;
+  height: 2rem;
+  border: none;
+  background: transparent;
+  & svg {
+    width: 100%;
+    height: 100%;
+    color: #ffffff;
+  }
+  ${media.medium`
+   display: block;
+       `}
 `;
